d2d69da8ef9965a037615041ea453c3df45b3717
riddman <riddleman@ukr.net>
2023-11-01 23:26:58 +0200

    kolourpaint was added

--- basic_installation.sh
+++ basic_installation.sh
@@ -3,6 +3,9 @@
 curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
 echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 sudo pacman -Syu sublime-text
+sudo pacman -S kolourpaint
+
+## AUR package  visual-studio-code-bin
 
 ## docker
 sudo pacman -S docker
